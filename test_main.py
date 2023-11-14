d22f009c880b56b98937ae166c6a9b4aadff50b1
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-11-14 22:52:24 +0000

    removed trigger from test

--- test_main.py
+++ test_main.py
@@ -1,21 +1,25 @@
 import os
+import requests
 from dotenv import load_dotenv
 from main import run_job
 
 load_dotenv()
 
 
-def test_run_job():
-    # Make request to Databricks API
+def test_env_vars():
+    # Get environment variables
     access_token = os.getenv("DATABRICKS_ACCESS_TOKEN")
     job_id = os.getenv("PIPELINE_JOB_ID")
     server_host_name = os.getenv("SERVER_HOSTNAME")
 
-    # Check if environment variables are set 
+    # Check if environment variables are set properly
     assert access_token is not None, "DATABRICKS_ACCESS_TOKEN is not set."
     assert job_id is not None, "PIPELINE_JOB_ID is not set."
     assert server_host_name is not None, "SERVER_HOSTNAME is not set."
 
-    response = run_job(access_token, job_id, server_host_name)
 
-    assert response == 200
+def test_csv_data_source():
+    url = "https://github.com/fivethirtyeight/data/raw/master/nba-draft-2015/historical_projections.csv"
+    # Check if the CSV file is accessible
+    response = requests.get(url)
+    assert response.status_code == 200, "CSV file is not accessible."
